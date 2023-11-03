27ad7ead3d55764f6dffd3e5be4c9c932d1365bd
Okpozu Otsima <otsimaokpozu@gmail.com>
2023-11-03 04:07:37 +0100

    Complete Visualization tab and filled it, create and connecte to db

--- database.py
+++ database.py
@@ -1,16 +1,77 @@
-import streamlit as st
-import plotly.graph_objects as go
-import calendar
-from datetime import datetime
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
 
-label = ["income", "total income", "other income", "rent", 'food']
-source = [0, 1, 2, 2]
-target = [2, 2, 3, 4]
-value = [10, 2, 6, 4]
+db = client['finance_records']
 
-link = dict(source=source, target=target, value=value)
-node = dict(label=label, pad=50, thickness=5)
-data= go.Sankey(link=link, node=node)
+coll = db['income_expense']
 
-fig = go.Figure(data)
-fig.show()
+
+# --- DATABASE INTERFACE ---
+# def get_all_periods():
+#     items = db.fetch_all_periods()
+#     periods = [item["key"] for item in items]
+#     return coll.insert_one(periods)
+
+
+# def init_connection():
+#     return MongoClient(**st.secrets["mongo"])
+
+# client = init_connection()
+db = client['my_db']
+coll = db["my_coll"]
+
+
+period_data = db.my_coll.find_one()
+
+print(period_data)
+periods = [item["key"] for item in period_data.items()]
+# comment = period_data.get("comment")
+# expenses = period_data.get("expenses")
+# incomes = period_data.get("incomes")
+
+print(periods)
+
+
+
+
+
+
+
+
+def get_all_periods():
+    items = db.my_coll.find()  # Find all documents in the collection
+    periods = [item["_id"] for item in items]  # Assuming "_id" is the period identifier
+    return periods
+
+if selected == "Data Visualization":
+    st.header("Data Visualization")
+    with st.form("saved periods"):
+        periods = get_all_periods()
+        if not periods:
+            st.warning("No saved periods found.")
+        else:
+            period = st.selectbox("Select Period:", periods)
+            submitted = st.form_submit_button("Plot Period")
+
+            if submitted:
+                # Get data for the selected period
+                period_data = db.my_coll.find_one({"_id": period})
+
+                if period_data:
+                    comment = period_data.get("comment")
+                    expenses = period_data.get("expenses")
+                    incomes = period_data.get("incomes")
+                    st.write("Comment:", comment)
+                    st.write("Expenses:", expenses)
+                    st.write("Incomes:", incomes)
+                else:
+                    st.warning("Selected period not found in the database.")
+
+
+
+
+
+                # ... Calculate metrics, create Sankey diagram, etc.
+
+            else:
+                st.warning("Selected period not found in the database.")
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 import calendar
 from datetime import datetime
 from streamlit_option_menu import option_menu
+from pymongo import MongoClient
 
 
 
@@ -39,6 +40,27 @@ hide_st_style = """
     footer  {visibility: hidden;}
     </style>
 """
+
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+db = client['my_db']
+coll = db["my_coll"]
+
+
+
+
+def get_all_periods():
+    items = db.my_coll.find()  # Find all documents in the collection
+    periods = [item["_id"] for item in items]  # Assuming "_id" is the period identifier
+    return periods
+
+
 st.markdown(hide_st_style, unsafe_allow_html=True)
 #   input and save periods
 if selected == "Data Entry":
@@ -57,7 +79,7 @@ if selected == "Data Entry":
             for expense in Expenses:
                 st.number_input(f"{expense}", format="%i", step=10, min_value=0, key=expense)
         with st.expander("Comment"):
-            st.text_area("", placeholder= "Enter a comment here . . .", )
+            comment_input= st.text_area("", placeholder= "Enter a comment here . . .", )
 
 
         submitted = st.form_submit_button("Save Data")
@@ -65,47 +87,63 @@ if selected == "Data Entry":
             period = str(st.session_state["year"]) + "_"+ str(st.session_state["month"])
             incomes = {income:st.session_state[income] for income in Incomes}
             expenses = {expense:st.session_state[expense] for expense in Expenses}
+            if "comment_input" in st.session_state:
+                comment = {'comment': st.session_state['comment_input']}
+                if comment:
+                    st.write("Comment:", comment)
             st.write(f"incomes:{incomes}")
             st.write(f"expense:{expenses}")
+            db.my_coll.insert_one({"period":period, 'expenses':expenses, 'incomes':incomes, 'comment':comment_input})
             st.success("Data Saved")
-        
-    # ------------------------------
 
-if selected== "Data Visualization":
+
+
+if selected == "Data Visualization":
     st.header("Data Visualization")
-    with st.form("saved periods"):
-        period= st.selectbox("Select Period:", ["2022_March"])
-        submitted = st.form_submit_button("plot Period")
-        if submitted:
-            comment = "Some Text"
-            incomes = {"Salary":1500, "Blog": 100, 'Other income':50}
-            expenses = {"Rent":600, "Utilities":200, "Groceries": 50, "car":100, 
-                            "Other savings":10, "savings":10}
+    with st.form("saved periods", clear_on_submit=True):
+        col1 = st.selectbox("Select Month", months, key="month")
+        col2 = st.selectbox('Select Year', year, key="year")
+        periods = f"{col2}_{col1}"
+
+        if not periods:
+            st.warning("No saved periods found.")
+        else:
+            # Get data for the selected period
+            period_data = db.my_coll.find_one({"period": periods}, {'_id': 0})
+
+            if period_data:
+                comment = period_data.get("comment")
+                expenses = period_data.get("expenses")
+                incomes = period_data.get("incomes")
                 
-        # Create metrics
+            # Create metrics
+                submitted = st.form_submit_button("Plot Period")
+                if submitted:
 
-            total_income = sum(incomes.values())
-            total_expenses = sum(expenses.values())
-            remaining_budget = total_income - total_expenses
-            col1, col2, col3 = st.columns(3)
-            col1.metric("Total Income", f"{total_income} {currency}")
-            col2.metric("Total Expenses", f"{total_expenses} {currency}")
-            col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
-            st.text(f"comment: {comment}")
+                    total_income = sum(incomes.values())
+                    total_expenses = sum(expenses.values())
+                    remaining_budget = total_income - total_expenses
+                    col1, col2, col3 = st.columns(3)
+                    col1.metric("Total Income", f"{total_income} {currency}")
+                    col2.metric("Total Expenses", f"{total_expenses} {currency}")
+                    col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
+                    st.text(f"comment: {comment}")
 
-            label = list(incomes.keys()) + [total_expenses] + list(expenses.keys())
-            source = list(range(len(incomes))) + [len(incomes)] * len (expenses)
-            target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses]
-            value = list(incomes.values()) + list(expenses.values())
+                    label = list(incomes.keys()) + [total_expenses] + list(expenses.keys())
+                    source = list(range(len(incomes))) + [len(incomes)] * len (expenses)
+                    target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses]
+                    value = list(incomes.values()) + list(expenses.values())
 
-            link = dict(source=source, target=target, value=value)
-            node = dict(label=label, pad=20, thickness=30, color='#E694FF')
-            data= go.Sankey(link=link, node=node)
+                    link = dict(source=source, target=target, value=value)
+                    node = dict(label=label, pad=20, thickness=30, color='#98f6d0')
+                    data= go.Sankey(link=link, node=node)
 
-            fig = go.Figure(data)
-            fig.update_layout(margin=dict(l=0,r=0,t=5,b=5))
-            st.plotly_chart(fig, use_container_width=True)
-            fig.show()
+                    fig = go.Figure(data)
+                    fig.update_layout(margin=dict(l=0,r=0,t=5,b=5))
+                    st.plotly_chart(fig, use_container_width=True)
+                    fig.show()
+                else:
+                    st.warning("Selected period not found in the database.")
 
 
 
